7ffef6140545a11d61d3b0e7a5d61960375fbff6
Oscar-117 <121317852+Oscar-117@users.noreply.github.com>
2022-12-24 01:40:09 -0600

    Add files via upload

---
+++ Controllers/TareasController.cs
@@ -0,0 +1,88 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tareas1MVC.Entidades;
+using Tareas1MVC.Models;
+using Tareas1MVC.Servicios;
+
+namespace Tareas1MVC.Controllers
+{
+    [Route("api/tareas")]
+    public class TareasController: ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IServicioUsuarios servicioUsuarios;
+        private readonly IMapper mapper;
+
+        public TareasController(ApplicationDbContext context, 
+            IServicioUsuarios servicioUsuarios,
+            IMapper mapper)
+        {
+            this.context = context;
+            this.servicioUsuarios = servicioUsuarios;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<TareaDTO>>> Get()
+        {
+            
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+
+            var tareas = await context.Tareas.
+                Where(t => t.UsuarioCreacionId == usuarioId)
+                .OrderBy(t => t.Orden)
+                .ProjectTo<TareaDTO>(mapper.ConfigurationProvider)
+                .ToListAsync();
+            return tareas;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Tarea>> Post([FromBody] string titulo)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var existenTareas = await context.Tareas.AnyAsync(t => t.UsuarioCreacionId == usuarioId);
+            var ordenMayor = 0;
+                if (existenTareas)
+            {
+                ordenMayor = await context.Tareas.Where(t => t.UsuarioCreacionId == usuarioId).Select(t => t.Orden).MaxAsync();
+            }
+            var tarea = new Tarea
+            {
+                Titulo = titulo,
+                UsuarioCreacionId = usuarioId,
+                FechaCreacion = DateTime.UtcNow,
+                Orden = ordenMayor + 1
+            };
+
+            context.Add(tarea);
+            await context.SaveChangesAsync();
+            return tarea;
+        }
+
+        [HttpPost("ordenar")]
+        public async Task<IActionResult> Ordenar([FromBody] int[] ids)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var tareas = await context.Tareas
+                .Where(t => t.UsuarioCreacionId == usuarioId).ToListAsync();
+            var tareasId = tareas.Select(t => t.Id);
+            var idsTareasNoPertenecenAlUsuario = ids.Except(tareasId).ToList();
+            if (idsTareasNoPertenecenAlUsuario.Any())
+            {
+                return Forbid();
+            }
+            var tareasDiccionario = tareas.ToDictionary(x => x.Id);
+            for (int i = 0; i < ids.Length; i++)
+            {
+                var id = ids[i];
+                var tarea = tareasDiccionario[id];
+                tarea.Orden = i + 1;
+            }
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+    
+}
