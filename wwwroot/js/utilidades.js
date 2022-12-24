c39d95973018208595fe873e52e85bd143cfe321
Oscar-117 <121317852+Oscar-117@users.noreply.github.com>
2022-12-24 01:50:55 -0600

    Add files via upload

---
+++ wwwroot/js/utilidades.js
@@ -0,0 +1,23 @@
+﻿async function manejarErrorApi(respuesta) {
+    let mensajeError = '';
+
+    if (respuesta.status === 400) {
+        mensajeError = await respuesta.text();
+    } else if (respuesta.status === 404) {
+        mensajeError = await respuesta.text();
+
+    } else {
+        mensajeError = await respuesta.text();
+    }
+
+    mostrarMensajeError(mensajeError);
+  
+}
+
+function mostrarMensajeError(mensaje) {
+    Swal.fire({
+        icon: 'error',
+        title: 'Error...',
+        text: mensaje
+        })
+}
