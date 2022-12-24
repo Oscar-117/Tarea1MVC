f687002afbac43ca974341fccf200327432795b7
Oscar-117 <121317852+Oscar-117@users.noreply.github.com>
2022-12-24 01:51:30 -0600

    Add files via upload

---
+++ Migrations/20221210080509_Pasos.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tareas1MVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class Pasos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Paso",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TareaId = table.Column<int>(type: "int", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Realizado = table.Column<bool>(type: "bit", nullable: false),
+                    Orden = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Paso", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Paso_Tareas_TareaId",
+                        column: x => x.TareaId,
+                        principalTable: "Tareas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Paso_TareaId",
+                table: "Paso",
+                column: "TareaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Paso");
+        }
+    }
+}
---
+++ Migrations/20221215084007_AdminRol.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tareas1MVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminRol : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"IF NOT EXISTS(Select Id From AspNetRoles Where Id = '35d2fd14-9705-486b-a14c-d7bb40fef811')
+BEGIN 
+	INSERT AspNetRoles (Id, [Name], [NormalizedName])
+	VALUES ('35d2fd14-9705-486b-a14c-d7bb40fef811', 'admin', 'ADMIN')
+
+END");
+        
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE AspNetRoles Where Id = '35d2fd14-9705-486b-a14c-d7bb40fef811'");
+
+        }
+    }
+}
